0e5df7c60390ea36e72020e2289056710caaed18
Jogurat123 <65083872+Jogurat123@users.noreply.github.com>
2021-02-12 16:02:16 +0100

    Final version of the project

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
 import sort
 
-final = sort.sorted
+i = 0
 
-print(final)
+while i < len(sort.sorted):
+    print(sort.sorted[i]["hrv"], sort.sorted[i]["cases"])
+    i += 1
+
+print("")
+i = 0
+
+while i < len(sort.sorted):
+    print(sort.sorted[i]["name"], sort.sorted[i]["cases"])
+    i += 1
+
+print("")
+i = 0
+
+while i < len(sort.sorted):
+    print(sort.sorted[i]["ger"], sort.sorted[i]["cases"])
+    i += 1
+
+print("")
+i = 0
+
+while i < len(sort.sorted):
+    print(sort.sorted[i]["ita"], sort.sorted[i]["cases"])
+    i += 1
+
+print("")
+i = 0
+
+while i < len(sort.sorted):
+    print(sort.sorted[i]["slo"], sort.sorted[i]["cases"])
+    i += 1
--- sort.py
+++ sort.py
@@ -7,70 +7,57 @@ import time
 #istra = input("input istria: ")
 
 #istria = countries.istria(istra)
-croatia = countries.croatia()
-norway = countries.norway()
-greece = countries.greece()
-albania = countries.albania()
-germany = countries.germany()
-denmark = countries.denmark()
-hungary = countries.hungary()
-poland = countries.poland()
-italy = countries.italy()
-slovakia = countries.slovakia()
+croatia = int(countries.croatia())
+norway = int(countries.norway())
+greece = int(countries.greece())
+albania = int(countries.albania())
+germany = int(countries.germany())
+denmark = int(countries.denmark())
+hungary = int(countries.hungary())
+poland = int(countries.poland())
+italy = int(countries.italy())
+slovakia = int(countries.slovakia())
 time.sleep(5)
-france = countries.france()
-macedonia = countries.macedonia()
-austria = countries.austria()
-spain = countries.spain()
-united_kingdom = countries.united_kingdom()
-portugal = countries.portugal()
-sweden = countries.sweden()
-netherlands = countries.netherlands()
-serbia = countries.serbia()
-switzerland = countries.switzerland()
+france = int(countries.france())
+macedonia = int(countries.macedonia())
+austria = int(countries.austria())
+spain = int(countries.spain())
+united_kingdom = int(countries.united_kingdom())
+portugal = int(countries.portugal())
+sweden = int(countries.sweden())
+netherlands = int(countries.netherlands())
+serbia = int(countries.serbia())
+switzerland = int(countries.switzerland())
 time.sleep(5)
-belgium = countries.belgium()
-slovenia = countries.slovenia()
-czechia = countries.czechia()
+belgium = int(countries.belgium())
+slovenia = int(countries.slovenia())
+czechia = int(countries.czechia())
 
-#countries_array = [croatia, norway, greece, albania, germany, denmark, hungary, poland, italy, slovakia, france, macedonia, austria, spain, united_kingdom, portugal, sweden, netherlands, serbia, switzerland, belgium, slovenia, czechia]
-
-def sort(to_sort):
-    for i in range(len(to_sort)):
-        swap = i + np.argmin(to_sort[i:]) 
-        (to_sort[i], to_sort[swap]) = (to_sort[swap], to_sort[i])
-    return(to_sort)
-
-
-
-lis = [{ "name" : "Croatia", "cases" : croatia}, 
-{ "name" : "Albania", "cases" : albania },
-{ "name" : "Norway", "cases" : norway },
-{ "name" : "Greece", "cases" : greece },
-{ "name" : "Germany", "cases" : germany },
-{ "name" : "Albania", "cases" : albania },
-{ "name" : "France" , "cases" : france },
-{ "name" : "Denmark" , "cases" : denmark },
-{ "name" : "Hungary" , "cases" : hungary },
-{ "name" : "Poland" , "cases" : poland },
-{ "name" : "Italy" , "cases" : italy },
-{ "name" : "Slovakia" , "cases" : slovakia },
-{ "name" : "Macedonia" , "cases" : macedonia },
-{ "name" : "Austria" , "cases" : austria },
-{ "name" : "Spain" , "cases" : spain },
-{ "name" : "United Kingdom" , "cases" : united_kingdom },
-{ "name" : "Portugal" , "cases" : portugal },
-{ "name" : "Sweden" , "cases" : sweden },
-{ "name" : "Netherlands" , "cases" : netherlands },
-{ "name" : "Serbia" , "cases" : serbia },
-{ "name" : "Switzerland" , "cases" : switzerland },
-{ "name" : "Belgium" , "cases" : belgium },
-{ "name" : "Slovenia" , "cases" : slovenia},
-{ "name" : "Czechia" , "cases" : czechia },]
-
- 
-# using sorted and lambda to print list sorted
-# by age 
+lis = [
+{ "name" : "Croatia: ", "cases" : croatia, "hrv" : "Hrvatska: ", "ger" : "Kroatien: ", "ita" :"Croazia: ", "slo" : "Hrvaška: "}, 
+{ "name" : "Albania: ", "cases" : albania, "hrv" : "Albania: ", "ger" : "Albanien: ", "ita" :"Albania: ", "slo" : "Albanija: " },
+{ "name" : "Norway: ", "cases" : norway, "hrv" : "Norveška: ", "ger" : "Norwegen: ", "ita" :"Norvegia: ", "slo" : "Norveška: " },
+{ "name" : "Greece: ", "cases" : greece, "hrv" : "Grčka: ", "ger" : "Griechenland: ", "ita" :"Grecia: ", "slo" : "Grčija: " },
+{ "name" : "Germany: ", "cases" : germany, "hrv" : "Njemačka: ", "ger" : "Deutschland: ", "ita" :"Germania: ", "slo" : "Nemčija: " },
+{ "name" : "France: " , "cases" : france, "hrv" : "Francuska: ", "ger" : "Frankreich: ", "ita" :"Francia: ", "slo" : "Francija: " },
+{ "name" : "Denmark: " , "cases" : denmark, "hrv" : "Danska: ", "ger" : "Danimarca: ", "ita" :"Danimarca: ", "slo" : "Danska: " },
+{ "name" : "Hungary: " , "cases" : hungary, "hrv" : "Mađarska: ", "ger" : "Ungarn: ", "ita" :"Ungheria: ", "slo" : "Madžarska: " },
+{ "name" : "Poland: " , "cases" : poland, "hrv" : "Poljska: ", "ger" : "Polen: ", "ita" :"Polonia: ", "slo" : "Poljska: " },
+{ "name" : "Italy: " , "cases" : italy, "hrv" : "Italija: ", "ger" : "Italien: ", "ita" :"Italia: ", "slo" : "Italija: " },
+{ "name" : "Slovakia: " , "cases" : slovakia, "hrv" : "Slovačka: ", "ger" : "Slowakei: ", "ita" :"Slovacchia: ", "slo" : "Slovaška: " },
+{ "name" : "Macedonia: " , "cases" : macedonia, "hrv" : "Makedonija: ", "ger" : "Mazedonien: ", "ita" :"Macedonia: ", "slo" : "Makedonija: " },
+{ "name" : "Austria: " , "cases" : austria, "hrv" : "Austrija: ", "ger" : "Österreich: ", "ita" :"Austria: ", "slo" : "Avstrija: " },
+{ "name" : "Spain: " , "cases" : spain, "hrv" : "Španjolska: ", "ger" : "Spanien: ", "ita" :"Spagna: ", "slo" : "Španija: " },
+{ "name" : "United Kingdom: " , "cases" : united_kingdom, "hrv" : "Ujedinjeno Kraljevstvo: ", "ger" : "Združeno kraljestvo: ", "ita" :"Regno Unito: ", "slo" : "Združeno kraljestvo: " },
+{ "name" : "Portugal: " , "cases" : portugal, "hrv" : "Portugal: ", "ger" : "Portugal: ", "ita" :"Portogallo: ", "slo" : "Portugalska" },
+{ "name" : "Sweden: " , "cases" : sweden, "hrv" : "Švedska: ", "ger" : "Schweden: ", "ita" :"Svezia: ", "slo" : "Švedska" },
+{ "name" : "Netherlands: " , "cases" : netherlands, "hrv" : "Nizozemska: ", "ger" : "Niederlande: ", "ita" :"Olanda: ", "slo" : "Nizozemska" },
+{ "name" : "Serbia: " , "cases" : serbia, "hrv" : "Srbija: ", "ger" : "Serbien: ", "ita" :"Serbia: ", "slo" : "Srbija" },
+{ "name" : "Switzerland: " , "cases" : switzerland, "hrv" : "Švicarska: ", "ger" : "Schweiz: ", "ita" :"Belgio: ", "slo" : "Belgija" },
+{ "name" : "Belgium: " , "cases" : belgium, "hrv" : "Belgija: ", "ger" : "Belgien: ", "ita" :"Belgio: ", "slo" : "Belgija" },
+{ "name" : "Slovenia: " , "cases" : slovenia, "hrv" : "Slovenija: ", "ger" : "Slowenien: ", "ita" :"Slovenia: ", "slo" : "Slovenija"},
+{ "name" : "Czechia: " , "cases" : czechia, "hrv" : "Češka: ", "ger" : "Tschechien: ", "ita" :"Repubblica Ceca: ", "slo" : "Češka" },]
 
 sorted = sorted(lis, key = lambda i: i['cases'])
- 
+
+
